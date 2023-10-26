728c451b55c24cda87a483f8ee0d9b67afe8babc
Yuan Zhang <zhangyuan@users.noreply.github.com>
2023-10-26 22:28:40 +0800

    send enter to remove

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 if [ "$1" = "run" ]; then
     set -euo pipefail
     (cd $HOME/actions-runner && ./config.sh --unattended --url "$REPO_URL" --token "$REGISTRATION_TOKEN" --name "${RUNNER_NAME}" --replace && ./run.sh)
-    (cd $HOME/actions-runner && ./config.sh --unattended remove)
+    (cd $HOME/actions-runner && echo | ./config.sh remove)
 else
     set -euo pipefail
     eval $@
